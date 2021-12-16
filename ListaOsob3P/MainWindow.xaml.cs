1c06f932977f7c405f7d5895ba2814d97be7d696
markup54 <marzenakupka@tlen.pl>
2021-12-16 13:41:19 +0100

    Bindowanie list view bez formularza

--- ListaOsob3P/MainWindow.xaml.cs
+++ ListaOsob3P/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,21 @@ namespace ListaOsob3P
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Osoba> ListaOsob { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            PrzygotujWiazanie();
+        }
+        private void PrzygotujWiazanie()
+        {
+            ListaOsob = new ObservableCollection<Osoba>();
+            ListaOsob.Add(new Osoba("Jaś", "Pomidor", "123456789"));
+            ListaOsob.Add(new Osoba("Asia", "Marchewka", "123456789"));
+            ListaOsob.Add(new Osoba("Ola", "Brzoskwinka", "123456789"));
+            ListaOsob.Add(new Osoba("Szymon", "Ogórek", "123456789"));
+            ListaOsob.Add(new Osoba("Antek", "Ananas", "123456789"));
+            DataContext = this;
         }
     }
 }
---
+++ ListaOsob3P/Osoba.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaOsob3P
+{
+    public class Osoba : ICloneable
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Telefon { get; set; }
+
+        public Osoba(string imie, string nazwisko, string telefon)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Telefon = telefon;
+        }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
